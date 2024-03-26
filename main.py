dae1a5de28d79454c8f9c3596d4b752563f85c2b
semzuu <mohamedkhm28+github@gmail.com>
2024-03-26 12:26:58 +0100

    DFS and BFS implementations done

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 from taquin import Taquin
 
 
-def Astar():
+def Astar(FState: Taquin, LState: Taquin) -> list:
+    return
+
+
+def DFS(FState: Taquin, LState: Taquin, limit=None) -> list:
+    visited = list()
+    Nvisited = [FState]
+    currentState = Nvisited.pop(0)
+    while currentState.compare(LState):
+        for state in currentState.transitions():
+            if state not in visited:
+                Nvisited.insert(0, state)
+        visited.append(currentState)
+        currentState = Nvisited.pop(0)
     pass
 
 
-def DFS():
-    pass
-
-
-def BFS():
-    pass
+def BFS(FState: Taquin, LState: Taquin) -> list:
+    visited = list()
+    Nvisited = [FState]
+    currentState = Nvisited.pop(0)
+    while currentState.compare(LState):
+        for state in currentState.transitions():
+            if state not in visited:
+                Nvisited.append(state)
+        visited.append(currentState)
+        currentState = Nvisited.pop(0)
+        pass
 
 
 def main():
-    game = Taquin()
-    game.show()
-    print('Reached final state:', game.check())
-    s = int(input('swap: '))
-    game.swap(s)
-    game.show()
+    final = [
+                1, 2, 3,
+                8, 0, 4,
+                7, 6, 5
+                ]
+    firstState = Taquin()
+    finalState = Taquin(final)
+    firstState.randomize()
+    firstState.show()
+    print(firstState.moves())
+    for state in firstState.transitions():
+        state.show()
 
 
 if __name__ == '__main__':
